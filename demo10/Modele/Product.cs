b8a6546b763405664522a62747ba81218b148bc0
fa6om <fa6om@LAPTOP-4OBBIE72>
2024-12-26 11:37:29 +0300

    Add project files.

---
+++ demo10/Modele/Product.cs
@@ -0,0 +1,56 @@
+﻿using demo10.Modele;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace demo10.Modele
+{
+    public class Product
+    {
+        public string Function(string name)
+        {
+            return"Prodct is "+name;
+        }
+        public string Function(string name, int price)
+        {
+
+            return "Prodct is " + name + " price is " + price;
+        }
+        public string Function(string name, int price , int qty)
+        {
+            int result = price * qty;
+
+            return "Prodct is " + name + " price is " + price+" Qty is ; "+qty+" total result is "+result;
+        }
+
+
+
+
+        //bonas 
+
+        public string name { get; set; }
+        public int price { get; set; }
+        public int qty { get; set; }
+        static List<Product> Productslst = new List<Product>();
+        public void AddProducts(string Name, int Price, int Qty)
+        {
+            Productslst.Add(new Product { name = Name, price = Price, qty = Qty });
+        }
+
+        public  void GetProduct()
+        {
+
+            Console.WriteLine(" -----------All prodct ------------");
+            foreach (Product cutmar in Productslst)
+            {
+                Console.WriteLine("Name: "+ cutmar.name +"   "+cutmar.price+"  KD "+" Qty:  "
+                    +cutmar.qty+ "  "+" total:  "+ (cutmar.price* cutmar.qty));
+            }
+        }
+    } }
+  
---
+++ demo10/Program.cs
@@ -0,0 +1,51 @@
+﻿
+//product 
+//3 function
+//1 par name >> product name :Ipone 
+//2name price >> Ipone 300 
+//3 name price qty
+//>> ipone 300kd 1500kd 
+//calss min program 
+//console.ReadLine 
+
+using demo10.Modele;
+using System;
+
+Product products = new Product();
+Console.WriteLine("enter the name of prodect ");
+products.name=( Console.ReadLine());
+Console.WriteLine("enter the price of prodect ");
+products.price = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the qty of prodect ");
+products.qty = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+products.AddProducts(products.name,products. price,products. qty);
+
+//Console.WriteLine(products.name, +products.price, products.qty, (products.price * products.qty));
+            
+ products.GetProduct();
+
+Product prd = new Product();
+int count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number of prodect");
+for (int i = 1; i <= count; i++)
+{
+    Console.WriteLine("Enter your product  ");
+    string Pname = Console.ReadLine();
+
+    Console.WriteLine("Enter price ");
+    int Pprice = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter Qty ");
+    int Pqty = Convert.ToInt32(Console.ReadLine());
+
+
+    prd.AddProducts(Pname, Pprice, Pqty);
+
+}
+products.GetProduct();
+
+    
